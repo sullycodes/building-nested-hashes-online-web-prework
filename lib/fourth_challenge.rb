6e635652681f09ab02d6124d836a5cc53f172bb0
Daniel Sullivan <dansullivan19@gmail.com>
2019-04-09 19:33:25 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,16 +5,18 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
-      ]
+        { :name => "Benvolio", :age => "17", :attittude => "worried" },
+        { :name => "Meercutio", :age => "18", :attittude => "hot-headed" }
+      ] # end hero_friends
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-        #your hashes here!
-      ]
+      :heroine_friends => [ 
+        {:name => "Steven", :age => "30", :attittude => "confused" },
+        {:name => "Nurse", :age => "44", :attittude => "worried" }
+      ] # end heroine_friends
    }
 }
 
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,46 @@
 def second_challenge
   epic_tragedy = {
+    
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {
+        :name => "Lord Montague"
+        :age => "53"
       }, 
+      
+      :matriarch => {
+        :name => "Lady Montague"
+        :age => "54"
+      },
+      
+      :hero => {
+        :name => "Romeo"
+        :age => "15"
+        :status => "alive"
+      },
+      
+      :hero_friends => []
+    }, #end Montague
+    
     :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+      
+      :patriarch => {
+        :name => "Lord Capulet"
+        :age => "50"
+      }, 
+      
+      :matriarch => {
+        :name => "Lady Capulet"
+        :age => "51"
+      }, 
+      
+      :heroine => {
+        :name => "Juliet"
+        :age => "15"
+        :status => "alive"
+      }, 
+      
+      :heroine_friends => []
+    } # end capulet
+    
+  } #epic tragedy end
 end
